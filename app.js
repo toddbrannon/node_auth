b4f23c97e6f9e6b797ec2687c962944f254bffc3
Todd Brannon <toddqbrannon@gmail.com>
2022-01-10 21:56:31 -0600

    added signup and login views

--- app.js
+++ app.js
@@ -7,13 +7,16 @@ const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 
+// middleware
+app.use(express.static('public'));
+
 const dbURI = 'mongodb+srv://todd_node_auth:NodeAuth470dd!@clusternodeauth.md59l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 mongoose.connect(dbURI)
     .then(() => console.log('connected to mongodb'))
     .catch(err => console.log(err)
 );
 
-app.get('/', (req, res) => res.send("Hello world"));
+app.get('/', (req, res) => res.render("Home"));
 
 app.listen(port, () => {
     console.log(`The server is running on port ${port}...`)
