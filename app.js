558a6f7bb4a759728218f25cc6297f983f6f676b
Todd Brannon <toddqbrannon@gmail.com>
2022-01-10 23:06:38 -0600

    added cookie route handlers in app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.set('view engine', 'ejs');
 
 // middleware
 app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
 
 const dbURI = 'mongodb+srv://todd_node_auth:NodeAuth470dd!@clusternodeauth.md59l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 mongoose.connect(dbURI)
@@ -19,6 +22,24 @@ mongoose.connect(dbURI)
 
 app.get('/', (req, res) => res.render('home'));
 app.use(authRoutes);app.use(authRoutes);
+
+// cookies
+app.get('/set-cookies', (req, res) => {
+
+    res.cookie('newUser', false)
+    res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true })
+    res.send('you got the cookies');
+
+})
+
+app.get('/read-cookies', (req, res) => {
+
+    const cookies = req.cookies;
+    console.log(cookies);
+
+    res.json(cookies); 
+
+})
 
 app.listen(port, () => {
     console.log(`The server is running on port ${port}...`)
