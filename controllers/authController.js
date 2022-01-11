bf880d2d8025176fb9b1853468fbd26d12d92aaf
Todd Brannon <toddqbrannon@gmail.com>
2022-01-10 23:35:10 -0600

    added function in authController to add token after user added to database

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const jwt = require('jsonwebtoken');
 
 // handle errors
 const handleErrors = (err) => {
@@ -24,6 +25,13 @@ const handleErrors = (err) => {
   return errors;
 }
 
+const maxAge = 2 * 24 * 60 * 60; // 3 days in seconds
+const createToken = (id) => {
+    return jwt.sign({ id }, 'trusponse node auth secret', {
+        expiredIn: maxAge;
+    })
+}
+
 // controller actions
 module.exports.signup_get = (req, res) => {
   res.render('signup');
@@ -38,7 +46,9 @@ module.exports.signup_post = async (req, res) => {
 
   try {
     const user = await User.create({ email, password });
-    res.status(201).json(user);
+    const token = createToken(user._id);
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(201).json({ user: user._id });
   }
   catch(err) {
     const errors = handleErrors(err);
