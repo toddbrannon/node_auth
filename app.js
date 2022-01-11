7af4bed5c0b2e6ebe52f81063919ebc93e1ad249
Todd Brannon <toddqbrannon@gmail.com>
2022-01-10 21:09:53 -0600

    add mongo connection

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
+
+const dbURI = 'mongodb+srv://todd_node_auth:NodeAuth470dd!@clusternodeauth.md59l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+mongoose.connect(dbURI)
+    .then(() => console.log('connected to mongodb'))
+    .catch(err => console.log(err)
+);
 
 app.get('/', (req, res) => res.send("Hello world"));
 
