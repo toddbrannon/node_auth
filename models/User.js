50bbfb5f6f535e599921df6aa1c02c5b3f294f97
Todd Brannon <toddqbrannon@gmail.com>
2022-01-19 07:39:23 -0600

    login setup started

--- models/User.js
+++ models/User.js
@@ -25,6 +25,20 @@ userSchema.pre('save', async function(next) {
   next();
 });
 
+// static method on user model to login user
+userSchema.statics.login = async function(email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password)
+    if (auth) {
+      return user;
+    }
+    throw Error('incorrect password');
+  }
+  throw Error('incorrect email');
+}
+
+
 const User = mongoose.model('user', userSchema);
 
 module.exports = User;
