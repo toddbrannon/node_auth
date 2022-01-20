b0d6aa6d0f62e0f268c2dea10c9893aaa15384af
Todd Brannon <toddqbrannon@gmail.com>
2022-01-20 15:26:04 -0600

    add MONGODB_ADD variable conn string to app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
 const { requireAuth, checkUser } = require('./middleware/authMiddleware');
+require('dotenv').config();
 
 const app = express();
 
